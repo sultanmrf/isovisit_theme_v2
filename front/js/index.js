1acdc485c6cee2ed2afa1c4b4bb7bf687bb8d75c
MohhamadRezaFanodi <sultanfanody@gmail.com>
2022-04-12 15:34:01 +0430

    edit scss and index.html add img support.png and ellipses and add index.js

---
+++ front/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){
+
+    $(document).on('click','.btn-patients',function (){
+        $check_expended = $(this).attr("aria-expanded");
+        debugger;
+        if($check_expended == 'true'){
+            $(this).parent().removeClass('bg-transparent').addClass('active_menu');
+            $(this).removeClass('unactive_menu');
+            $('.btn-patients i').addClass('fa-chevron-up').removeClass('fa-chevron-down')
+        }else{
+            $(this).parent().removeClass('active_menu').addClass(['bg-transparent','iso-color-blur-dark']);
+            $(this).addClass('unactive_menu');
+            $('.btn-patients i').addClass('fa-chevron-down').removeClass('fa-chevron-up')
+        }
+
+    })
+});
