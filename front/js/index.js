9bf6027a2660dc83070c07627a65e617ce1134e6
MohhamadRezaFanodi <sultanfanody@gmail.com>
2022-04-26 15:36:53 +0430

    edit scss footer classes main and index.html and js and add image to folder certificates and icons

--- front/js/index.js
+++ front/js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function (){
     $(document).on('click','.btn-patients',function (){
-        $check_expended = $(this).attr("aria-expanded");
-        if($check_expended == 'true'){
+        let val_aria_expanded = $(this).attr("aria-expanded");
+        if(val_aria_expanded == 'true'){
             $(this).parent().removeClass('bg-transparent').addClass('active_menu');
             $(this).removeClass('unactive_menu');
             $(this).find('i').addClass('fa-chevron-up').removeClass('fa-chevron-down');
@@ -10,15 +10,15 @@ $(document).ready(function (){
             $(this).addClass('unactive_menu');
             $(this).find('i').addClass('fa-chevron-down').removeClass('fa-chevron-up');
         }
-
     })
 
+    /* swiper main page */
     new Swiper(".swiper-question-answer", {
         slidesPerView: 1,
         spaceBetween: 8,
         pagination: {
             el: ".swiper-pagination-question-answer",
-            paginationClickable: true,
+            clickable: true,
         },
     });
 
@@ -27,7 +27,7 @@ $(document).ready(function (){
         spaceBetween: 8,
         pagination: {
             el: ".swiper-pagination-article",
-            paginationClickable: true,
+            clickable: true,
         },
     });
 
@@ -38,23 +38,57 @@ $(document).ready(function (){
 
 
     new Swiper(".swiper-medicines", {
-        slidesPerView: "auto",
+        slidesPerView:"auto",
         spaceBetween: 16,
         pagination: {
             el: ".swiper-pagination-medicines",
-            paginationClickable: true,
+            clickable: true,
         },
     });
 
     new Swiper(".swiper-certificates", {
-        slidesPerView: 0,
-        centeredSlides: true,
-        spaceBetween: 30,
-        grabCursor: true,
+        slidesPerView:2,
+        spaceBetween: 10,
         pagination: {
             el: ".swiper-pagination-certificates",
             clickable: true,
         },
     });
+
+
+    /* section communication-iso */
+    $(".communication-iso img").click(function (){
+        let val_aria_expanded = $(this).attr('aria-expanded');
+        if(val_aria_expanded == 'true'){
+            $(".dropdown-menu-communication li").css('opacity','0');
+            $(this).attr('aria-expanded','false');
+            $(this).attr('src','./images/icons/icon-support-light.png')
+                .addClass('rotate-0deg')
+                .removeClass('rotate-180deg');
+
+        }else{
+            $(".dropdown-menu-communication li").css('opacity','1');
+            $(this).attr('aria-expanded','true');
+            $(this).attr('src','./images/icons/close.png')
+                .addClass('rotate-180deg')
+                .removeClass('rotate-0deg');
+        }
+    })
+
+    /* section scroll top */
+
+    $(window).scroll(function(){
+        var scrolled_val = $(this).scrollTop();
+        if(scrolled_val > 1500){
+            $(".scroll-top").addClass('d-flex');
+        }else{
+            $(".scroll-top").removeClass('d-flex');
+        }
+    });
+
+    $(document).on('click','.scroll-top',function (){
+        $('html').animate({scrollTop : 0},200);
+    });
+
 
 });
